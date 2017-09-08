1052396233ad9c8396ff29d89893ec9212807b6a
Stefan Dindyal <paradox880@gmail.com>
2017-09-07 23:00:04 -0400

    update

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,11 @@
 	const features = $('#featuresSlider'),
 		howto = $('#howtoSlider'),
 		toggle = $('.toggle-box .toggle'),
+		howtoCount = howto.find('img').length,
 		timing = 300;
 	
 	features.owlCarousel({
-	    loop:true,
+	    loop:false,
 	    nav:false,
 	    mouseDrag:false,
 	    touchDrag:false,
@@ -15,13 +16,35 @@
 	    animateOut: 'slideOutUp'
 	});
 
+	howto.parent().append('<div class="dot-nav"><div class="inner"></div></div>');
+	for(i=0; i< howtoCount; i++){
+		$('.dot-nav .inner').append('<div class="dot-dot"><span></span></div>');	
+	}
+	$('.dot-nav').append('<div class="bar"><div class="inner"></div></div>');
+	$('.dot-dot').on('click', function(e){
+		const el = $(this),
+			index = el.index();
+		howto.trigger('to.owl.carousel',[index, 300]);
+	});
+
 	howto.owlCarousel({
 	    loop:true,
 	    nav:false,
-	    mouseDrag:false,
-	    touchDrag:false,
-	    items:1
-	});
+	    dots:false,
+	    mouseDrag:true,
+	    touchDrag:true,
+	    items:1,
+	    autoplay:true,
+	    autoplayTimeout:4000,
+	    onInitialized: function(event){
+	    	console.log(event);
+	    	$('.dot-dot').eq(0).addClass('active');
+	    },
+	    onTranslate: moveBar,
+	    onTranslated: function(event){
+
+	    }
+	});	
 	
 	toggle.first().addClass('selected').prev().addClass('noBorder');		
 	toggle.first().find('.toggle-body').css({height:toggle.first().find('.inner').outerHeight()});
@@ -37,5 +60,37 @@
 		el.find('.toggle-body').animate({height:innerHeight},{duration:timing,queue:false});
 		features.trigger('to.owl.carousel',[index,timing]);
 	});
+
+	function moveBar(event){
+		const el = howto,
+			items = event.item.count,
+    		item = event.item.index,
+    		dots = $('.dot-nav'),
+    		bar = $('.bar');
+    	console.log(item);
+    	if(item == 2){
+    		$('.dot-dot').removeClass('active');
+    		dots.find('.dot-dot').eq(0).addClass('active');
+    		bar.css('width','0%');
+    	} else if(item == 3){
+    		$('.dot-dot').removeClass('active');
+    		dots.find('.dot-dot').eq(0).addClass('active');
+    		dots.find('.dot-dot').eq(1).addClass('active');
+    		bar.css('width','42%');
+    	} else if(item == 4){
+    		$('.dot-dot').removeClass('active');
+    		dots.find('.dot-dot').eq(0).addClass('active');
+    		dots.find('.dot-dot').eq(1).addClass('active');
+    		dots.find('.dot-dot').eq(2).addClass('active');
+    		bar.css('width','72%');
+    	} else if(item == 5){
+    		$('.dot-dot').removeClass('active');
+    		dots.find('.dot-dot').eq(0).addClass('active');
+    		dots.find('.dot-dot').eq(1).addClass('active');
+    		dots.find('.dot-dot').eq(2).addClass('active');
+    		dots.find('.dot-dot').eq(3).addClass('active');
+    		bar.css('width','100%');
+    	}
+	}
 
 })(jQuery);
