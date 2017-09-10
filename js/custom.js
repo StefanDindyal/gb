e5062fa0a62cda962bfbff45e442a7db274c702a
Stefan Dindyal <paradox880@gmail.com>
2017-09-10 18:54:09 -0400

    switched to bxslider

--- js/custom.js
+++ js/custom.js
@@ -1,89 +1,80 @@
 !(function($){
+
 	// homepage
 	const features = $('#featuresSlider'),
 		howto = $('#howtoSlider'),
 		toggle = $('.toggle-box .toggle'),
 		howtoCount = howto.find('img').length,
 		timing = 300;
-	
-	features.owlCarousel({
-	    loop:false,
-	    nav:false,
-	    mouseDrag:false,
-	    touchDrag:false,
-	    items:1,
-	    animateIn: 'slideInUp',
-	    animateOut: 'slideOutUp'
+
+	// features slider Start
+	features.bxSlider({
+		mode: 'vertical',
+		pager: false,
+		controls: false,
+		touchEnabled: false		
 	});
-
-	howto.parent().append('<div class="dot-nav"><div class="inner"></div></div>');
-	for(i=0; i< howtoCount; i++){
-		$('.dot-nav .inner').append('<div class="dot-dot"><span></span></div>');	
-	}
-	$('.dot-nav').append('<div class="bar"><div class="inner"></div></div>');
-	$('.dot-dot').on('click', function(e){
-		const el = $(this),
-			index = el.index();
-		howto.trigger('to.owl.carousel',[index, 300]);
-	});
-
-	howto.owlCarousel({
-	    loop:true,
-	    nav:false,
-	    dots:false,
-	    mouseDrag:true,
-	    touchDrag:true,
-	    items:1,
-	    autoplay:true,
-	    autoplayTimeout:4000,
-	    onInitialized: function(event){
-	    	console.log(event);
-	    	$('.dot-dot').eq(0).addClass('active');
-	    },
-	    onTranslate: moveBar,
-	    onTranslated: function(event){
-
-	    }
-	});	
-	
 	toggle.first().addClass('selected').prev().addClass('noBorder');		
 	toggle.first().find('.toggle-body').css({height:toggle.first().find('.inner').outerHeight()});
-	
 	toggle.on('click', function(e){
 		const 
 		el = $(this),
+		selected = $('.toggle.selected').index(),
 		index = el.index(),
 		innerHeight = el.find('.inner').outerHeight();
+		features.goToSlide(index);
 		toggle.removeClass('selected noBorder');
 		toggle.find('.toggle-body').animate({height:0},{duration:timing,queue:false});
 		el.addClass('selected').prev().addClass('noBorder');		
-		el.find('.toggle-body').animate({height:innerHeight},{duration:timing,queue:false});
-		features.trigger('to.owl.carousel',[index,timing]);
+		el.find('.toggle-body').animate({height:innerHeight},{duration:timing,queue:false});						
 	});
+	// features slider End
 
-	function moveBar(event){
+	// howto slider Start
+	howto.parent().append('<div class="dot-nav"><div class="inner"></div><div class="navOver"><div class="inner"></div></div><div class="bar"><div class="inner"></div></div></div>');
+	for(i=0; i< howtoCount; i++){
+		$('.dot-nav > .inner, .dot-nav .navOver .inner').append('<div class="dot-dot"><span></span></div>');	
+	}
+	howto.bxSlider({
+		mode: 'horizontal',
+		pager: false,
+		controls: false,
+		touchEnabled: false,
+		auto: true,
+		pause: 4000,
+		onSliderLoad: function(){
+			$('.dot-dot').eq(0).addClass('active');
+		},
+		onSlideBefore: function($slideElement, oldIndex, newIndex){
+			moveBar($slideElement, oldIndex, newIndex);
+		}
+	});
+	$('.dot-dot').on('click', function(e){
+		const el = $(this),
+			index = el.index();
+		howto.goToSlide(index);
+	});
+	function moveBar($slideElement, oldIndex, newIndex){
 		const el = howto,
-			items = event.item.count,
-    		item = event.item.index,
+    		item = newIndex,
     		dots = $('.dot-nav'),
     		bar = $('.bar');
-    	console.log(item);
-    	if(item == 2){
+    	if(item == 0){
     		$('.dot-dot').removeClass('active');
     		dots.find('.dot-dot').eq(0).addClass('active');
     		bar.css('width','0%');
-    	} else if(item == 3){
+    	} else if(item == 1){
     		$('.dot-dot').removeClass('active');
     		dots.find('.dot-dot').eq(0).addClass('active');
     		dots.find('.dot-dot').eq(1).addClass('active');
     		bar.css('width','42%');
-    	} else if(item == 4){
+    	} else if(item == 2){
     		$('.dot-dot').removeClass('active');
     		dots.find('.dot-dot').eq(0).addClass('active');
     		dots.find('.dot-dot').eq(1).addClass('active');
     		dots.find('.dot-dot').eq(2).addClass('active');
     		bar.css('width','72%');
-    	} else if(item == 5){
+    	} else if(item == 3){
     		$('.dot-dot').removeClass('active');
     		dots.find('.dot-dot').eq(0).addClass('active');
     		dots.find('.dot-dot').eq(1).addClass('active');
@@ -92,5 +83,6 @@
     		bar.css('width','100%');
     	}
 	}
+	// howto slider End
 
 })(jQuery);
